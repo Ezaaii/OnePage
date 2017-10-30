348bbed4a694c35d6003b042699fc2bbfb384827
Unknown <n.vanroost@gmail.com>
2017-10-30 14:55:11 +0100

    update script in html + scroll on ID

---
+++ app.js
@@ -0,0 +1,31 @@
+/////////////////////
+// Scroll animation//
+/////////////////////
+var homelink = document.querySelector('#Accueil_link');
+var home = document.querySelector('#Accueil');
+var portflink = document.querySelector('#Portfolio_link');
+var portf = document.querySelector('#Portfolio');
+var contlink = document.querySelector('#Contact_link');
+var cont = document.querySelector('#Contact');
+
+homelink.addEventListener("click", function(){
+  window.scroll({
+    top : home.offsetTop,
+    left : 0,
+    behavior : 'smooth'
+  });
+});
+portflink.addEventListener("click", function(){
+  window.scroll({
+    top : portf.offsetTop,
+    left : 0,
+    behavior : 'smooth'
+  });
+});
+contlink.addEventListener("click", function(){
+  window.scroll({
+    top : cont.offsetTop,
+    left : 0,
+    behavior : 'smooth'
+  });
+});
